e4a039a7ec3bfee381c5ee39d4be6705e2cbfea7
Steliyan03 <rumenovsteliyan0@gmail.com>
2024-11-12 17:58:44 +0200

    Add files via upload

---
+++ Chess board/Chess board/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Chess_board
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalSquares = 64;
+            ulong totalGrains = 0;
+
+            Console.WriteLine("Grains of wheat on each square of the chessboard:");
+
+            // Преминете през всеки квадрат и изчислете броя на зърната
+            for (int i = 1; i <= totalSquares; i++)
+            {
+                ulong grainsOnSquare = (ulong)Math.Pow(2, i - 1);
+                totalGrains += grainsOnSquare;
+
+                // Изведете броя на зърната в текущия квадрат
+                Console.WriteLine($"Square {i}: {grainsOnSquare} grains");
+            }
+
+            // Изведете общия брой зърна на шахматната дъска
+            Console.WriteLine($"\nTotal number of grains on the chessboard: {totalGrains}");
+        }
+    }
+}
